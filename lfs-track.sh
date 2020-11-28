a9f67182e6ceeb71ec7b30caf4f4b9460d260c3d
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-11-28 10:23:25 -0500

    Initial commit

---
+++ lfs-track.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git lfs track "*.jpg"
+git lfs track "*.gltf"
+git lfs track "*.glb"
+git lfs track "*.bin"
+git lfs track "*.blend"
+git lfs track "*.png"
+git lfs track "*.xz"
+git lfs track "*.stl"
+git lfs track "*.escn"
+git lfs track "*.material"
+git lfs track "'*.material'"

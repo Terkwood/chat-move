f8fb811ea1b2115eaac794d80cfd99f2fd5ff4bc
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-11-29 11:03:27 -0500

    fix input

--- client/Player.gd
+++ client/Player.gd
@@ -6,14 +6,12 @@ var velocity = Vector2()
 puppet var puppet_position
 puppet var puppet_velocity = Vector2()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	if is_network_master():
 		$NameLabel.text = "You"
 	else:
 		var player_id = get_network_master()
 		$NameLabel.text = net.player_names_by_id[player_id]
-		
 		puppet_position = position
 
 
@@ -21,13 +19,13 @@ func _process(delta):
 	if is_network_master():
 		var move_dir = Vector2()
 		
-		if Input.is_action_pressed("up"):
+		if Input.is_action_pressed("ui_up"):
 			move_dir.y -= 1
-		if Input.is_action_pressed("down"):
+		if Input.is_action_pressed("ui_down"):
 			move_dir.y += 1
-		if Input.is_action_pressed("left"):
+		if Input.is_action_pressed("ui_left"):
 			move_dir.x -= 1
-		if Input.is_action_pressed("right"):
+		if Input.is_action_pressed("ui_right"):
 			move_dir.x += 1
 		
 		velocity = move_dir.normalized() * SPEED
@@ -47,4 +45,4 @@ func _process(delta):
 		# their position again. If we don't update puppet_pos to position after moving,
 		# we will keep jumping back until controlling player sends next position update.
 		# Therefore, we update puppet_pos to minimize jitter problems
-		puppet_pos = position
+		puppet_position = position

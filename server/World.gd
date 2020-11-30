b5aa89be55c7b724261e789f33875f4a8ed380ba
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-11-30 16:06:55 -0500

    Trim comments

--- server/World.gd
+++ server/World.gd
@@ -5,10 +5,9 @@ onready var Player = load("res://Player.tscn")
 
 puppetsync func spawn_player(spawn_pos, id):
 	var player = Player.instance()
-	
 	player.position = spawn_pos
-	player.name = String(id) # Important
-	player.set_network_master(id) # Important
+	player.name = String(id)
+	player.set_network_master(id)
 	
 	$Players.add_child(player)
 

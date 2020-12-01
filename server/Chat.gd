3a864a8c7287e4c814102f890333ad5665af97d1
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-01 12:17:19 -0500

    Add some chat

---
+++ server/Chat.gd
@@ -0,0 +1,19 @@
+extends Panel
+
+onready var ChatDisplay = $Display
+onready var ChatInput = $Input
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ENTER:
+			send_message()
+
+func send_message():
+	var msg = ChatInput.text
+	ChatInput.text = ""
+	var id = get_tree().get_network_unique_id()
+	rpc("receive_message", id, msg)
+
+sync func receive_message(id, msg):
+	ChatDisplay.text += "\n" + str(id) + ": " + msg
+	ChatDisplay.set_v_scroll(100.0)

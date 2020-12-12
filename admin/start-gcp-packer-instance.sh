cd2db3a94d764b907cf10334a2bafef31e8d3d42
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-12 13:32:28 -0500

    update script gp

--- admin/start-gcp-packer-instance.sh
+++ admin/start-gcp-packer-instance.sh
@@ -13,11 +13,12 @@ set -e      # bail on error
 : {$GCP_NETWORK:Requires GCP_NETWORK}
 : {$GCP_ZONE:Requires GCP_ZONE}
 : {$SERVICE_USER:Requires SERVICE_USER (name)}
+: {$INSTANCE_NAME:Requires INSTANCE_NAME}
 
 IMAGE_FAMILY=fedora-coreos-stable 
 IMAGE_NAME=fedora-coreos-32-20201104-3-0-gcp-x86-64 
 
-gcloud compute instances create INSTANCE-NAME \
+gcloud compute instances create $INSTANCE_NAME \
   --project $GCP_PROJECT \
   --image-family $IMAGE_FAMILY \
   --image-project $IMAGE_NAME \

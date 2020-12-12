c6e7002882e0914829a962086c9f2d7c2b5b1578
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-12 07:13:29 -0500

    Add script to export server using docker-godot-export (#11)

---
+++ server/export.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+GODOT_VERSION="3.2.3"
+
+docker run -e EXPORT_NAME="Linux/X11" -e OUTPUT_FILENAME="chat_move_server" -v $(pwd):/build/src -v target:/build/output gamedrivendesign/godot-export:$GODOT_VERSION

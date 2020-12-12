08a5b8f86d2b2b59ee0a8ca7575c22d6ddc365c6
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-12 07:12:12 -0500

    fix script

--- server/export.sh
+++ server/export.sh
@@ -2,4 +2,4 @@
 
 GODOT_VERSION="3.2.3"
 
-docker run -e EXPORT_NAME="Linux/X11" -e OUTPUT_FILENAME="chat_move_server" -v $(pwd):/build/src -v ./target:/build/output gamedrivendesign/godot-export:$GODOT_VERSION
+docker run -e EXPORT_NAME="Linux/X11" -e OUTPUT_FILENAME="chat_move_server" -v $(pwd):/build/src -v target:/build/output gamedrivendesign/godot-export:$GODOT_VERSION

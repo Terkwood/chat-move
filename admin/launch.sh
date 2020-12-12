e83b28f43f7ef398f5fc8cd8d482bf6a9d52103d
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-12 15:34:15 -0500

    hack up script

--- admin/launch.sh
+++ admin/launch.sh
@@ -4,6 +4,17 @@
 
 set -e
 
+: {$GCP_ZONE:Requires GCP_ZONE}
+
 STREAM='stable'
-VM_NAME='fcos-node01'
-gcloud compute instances create --image-project "fedora-coreos-cloud" --image-family "fedora-coreos-${STREAM}" "${VM_NAME}"
+INSTANCE_NAME='chat-move-server'
+
+# see https://docs.fedoraproject.org/en-US/fedora-coreos/provisioning-gcp/
+IMAGE_PROJECT=fedora-coreos-cloud 
+IMAGE_FAMILY=fedora-coreos-stable 
+
+gcloud compute instances create $INSTANCE_NAME \
+  --image-family $IMAGE_FAMILY \
+  --image-project $IMAGE_PROJECT \
+  --zone $GCP_ZONE \
+  --machine-type f1-micro
